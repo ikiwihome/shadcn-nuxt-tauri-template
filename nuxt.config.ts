5b8abd931c411b732e22ac2bf12373c42a5be9ae
ikiwi <ikiwicc@gmail.com>
2025-05-22 10:51:23 +0800

    init

---
+++ nuxt.config.ts
@@ -0,0 +1,6 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    compatibilityDate: '2025-05-22',
+    devtools: { enabled: false },
+    ssr: false // SSR must be turned off
+})
